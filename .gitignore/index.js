ddfa0fe1f0f72648b44f5a45b1c4a54725ae9b26
Tenweex <48263995+Tenweex@users.noreply.github.com>
2019-05-08 00:31:25 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,10 +5,10 @@ var prefix = ("*")
 
 bot.on('ready', function() {
     bot.user.setGame("Command: *help");
-    console.log("Connectedç");
+    console.log("Connected");
 });
 
-bot.login("NTc1NDM1MDg0NzE1MjYxOTUy.XNH6BQ.nh2t56dN4UKVqjs8ph0rMSZZDxo");
+bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {
